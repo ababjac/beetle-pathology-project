a77c41f346534dc82e65eced406a458a470680d6
Ashlenator <ababjac@gmail.com>
2022-02-13 20:01:05 -0500

    updating scripts

---
+++ scripts/fill_na.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('data/Co-evolution_Data_Nov2021/Datasets_Codes_distantace_matrices/genalex_wtb_male_female.csv')
--- scripts/generate_nei_distance_matrix.py
+++ scripts/generate_nei_distance_matrix.py
@@ -2,9 +2,18 @@ import pandas as pd
 import numpy as np
 from rpy2.robjects.packages import importr
 from rpy2.robjects import pandas2ri
+#import os
+#import rpy2.situation
+
+
+#os.environ['PATH'] = '/home/ababjac/R/x86_64-pc-linux-gnu-library/4.1' + os.pathsep + os.environ.get('PATH', '')
+#for row in rpy2.situation.iter_info():print(row)
 
 pandas2ri.activate()
 
 poppr = importr('poppr')
 
 data = pd.read_csv('data/Co-evolution_Data_Nov2021/Datasets_Codes_distantace_matrices/genalex_wtb_male_female.csv')
+#print(data)
+
+#deleted the rest until can get r environment path configured with Python to call poppr.nei.dist()
